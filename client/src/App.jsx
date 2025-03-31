e525e236e7fe0d18813686093fc9f5bb55224a51
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2025-03-31 13:14:00 +0800

    feat: implement fetchTasks functions in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:3000/tasks");
+      console.log(data);
+    } catch (err) {
+      console.error("Failed to fetch tasks", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return <></>;
 };
 
