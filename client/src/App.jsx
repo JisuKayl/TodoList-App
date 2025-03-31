be6c74a1b6e9d883cb1088919f657170c3fa7c72
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2025-03-31 13:47:13 +0800

    feat: created useState for tasks to display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+
   const fetchTasks = async () => {
     try {
       const { data } = await axios.get("http://localhost:3000/tasks");
-      console.log(data);
+      setTasks(data);
     } catch (err) {
       console.error("Failed to fetch tasks", err);
     }
@@ -16,7 +18,19 @@ const App = () => {
     fetchTasks();
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              Title: {task.title} - Description: {task.description}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default App;
