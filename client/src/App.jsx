59757462f6ab23ed4340271fc401555920e4cf8c
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2025-04-10 21:59:08 +0800

    feat: complete crud in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,15 @@ const App = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  const clearAllFields = () => {
+    try {
+      setTitle("");
+      setDescription("");
+    } catch (err) {
+      console.error("Failed to clear fields", err);
+    }
+  };
+
   const fetchTasks = async () => {
     try {
       const { data } = await axios.get("http://localhost:3000/tasks");
@@ -16,14 +25,40 @@ const App = () => {
     }
   };
 
+  const fetchTaskById = async (id) => {
+    if (!id) return;
+
+    try {
+      const { data } = await axios.get(`http://localhost:3000/tasks/${id}`);
+      console.log(data);
+      setTitle(data.title);
+      setDescription(data.description);
+    } catch (err) {
+      console.error("Failed to fetch task", err);
+    }
+  };
+
   const addTask = async () => {
     try {
       await axios.post("http://localhost:3000/tasks", { title, description });
-      setTitle("");
-      setDescription("");
+      clearAllFields();
       fetchTasks();
     } catch (err) {
       console.error("Failed to add tasks", err);
+    }
+  };
+
+  const updateTask = async (id) => {
+    if (!id) return;
+    try {
+      await axios.put(`http://localhost:3000/tasks/${id}`, {
+        title,
+        description,
+      });
+      clearAllFields();
+      fetchTasks();
+    } catch (err) {
+      console.error("Failed to update task", err);
     }
   };
 
@@ -33,6 +68,16 @@ const App = () => {
       fetchTasks();
     } catch (err) {
       console.error("Failed to delete all tasks", err);
+    }
+  };
+
+  const deleteTaskById = async (id) => {
+    if (!id) return;
+    try {
+      await axios.delete(`http://localhost:3000/tasks/${id}`);
+      fetchTasks();
+    } catch (err) {
+      console.error("Failed to delete task", err);
     }
   };
 
@@ -57,11 +102,22 @@ const App = () => {
       />
       <button onClick={addTask}>Add Task</button>
       <button onClick={deleteAllTasks}>Delete All Tasks</button>
+      <button onClick={clearAllFields}>Clear</button>
       <ul>
         {tasks.map((task, index) => {
           return (
             <li key={task.id}>
               {index + 1}. Title: {task.title} - Description: {task.description}
+              <button onClick={() => fetchTaskById(task.id)}>View</button>
+              <button
+                onClick={() => {
+                  updateTask(task.id);
+                }}
+              >
+                Update
+              </button>
+              <button onClick={() => deleteTaskById(task.id)}>Delete</button>
+              <button></button>
             </li>
           );
         })}
