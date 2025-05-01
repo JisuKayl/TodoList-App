de715dcdc6f5ea2c231e4a17f268a3d5d7b00116
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2025-05-01 14:41:43 +0800

    feat: add confirmation message before adding or updating task

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -46,6 +46,9 @@ const TaskForm = ({
         return;
       }
 
+      const confirmAddTask = confirm("Are you sure you want to add this task?");
+      if (!confirmAddTask) return;
+
       await addTask(title, description);
       alert("Task added successfully!");
       resetFormState();
@@ -68,6 +71,11 @@ const TaskForm = ({
         alert("Title is already taken. Please choose a different title.");
         return;
       }
+
+      const confirmUpdateTask = confirm(
+        "Are you sure you want to update this task?"
+      );
+      if (!confirmUpdateTask) return;
 
       await updateTask(selectedTask.id, title, description);
       alert("Task updated successfully!");
